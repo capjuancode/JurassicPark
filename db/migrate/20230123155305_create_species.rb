1a3349c3f35301dc6b52b167525ae1e91a971c18
Juan Lopez <capjuancode@gmail.com>
2023-01-23 10:57:09 -0500

    created species

---
+++ db/migrate/20230123155305_create_species.rb
@@ -0,0 +1,10 @@
+class CreateSpecies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :species do |t|
+      t.string :name, null: false
+      t.integer :diet, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/species.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :species do
+    name { "MyString" }
+    diet { 1 }
+  end
+end
