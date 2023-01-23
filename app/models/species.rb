15a53835938e43124cbc3461d001c9d595d00607
Juan Lopez <capjuancode@gmail.com>
2023-01-23 11:53:04 -0500

    sample seeds not complete

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,5 @@
 class Species < ApplicationRecord
   has_many :dinos
+
+  enum diet: { 'carnivore': 0, 'herbivore': 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+%w[Tyrannosaurus Spinosaurus Megalosaurus].each do |name|
+  Species.create(name: name, diet: :carnivore)
+end
+
+%w[Brachiosaurus Stegosaurus Ankylosaurus Triceratops].each do |name|
+  Species.create(name: name, diet: :herbivore)
+end
+
+Species.where(diet: :carnivore).each do |species|
+  cage = Cage.create(name: species.name.pluralize, max_capacity: 2)
+  cage << Dino.create(name: species.name.first(6), species: species)
+end
+
+cage = Cage.create(name: 'Herbivores', max_capacity: 20)
+Species.where(diet: :herbivore).each do |species|
+  cage << Dino.create(name: species.name.first(6), species: species)
+end
+
+velociraptor = Species.create(name: 'Velociraptor', diet: :carnivore)
+cage = Cage.create(name: velociraptor.name.pluralize, max_capacity: 5)
+
+%w[Delta Echo Charlie Blue].each do |name|
+  cage << Dino.create(name: name, species: velociraptor)
+end
